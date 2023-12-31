b99595f7fa63aac1e50ea9502671e839b7bb2f3a
Ricado <rjfloresc14@gmail.com>
2023-12-30 21:58:09 -0600

    arreglos

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ async function searchMovies() {
 
     // Verifica si se ha ingresado un título
     if (movieTitle.trim() === '') {
-      alert('Por favor, ingrese un título de película.');
-      return;
+        alert('Por favor, ingrese un título de película.');
+        return;
     }
 
     try {
       // Realiza la búsqueda utilizando Fetch y promesas
-      const response = await fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${apiKey}`);
-      const data = await response.json();
+        const response = await fetch(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${apiKey}`);
+        const data = await response.json();
 
       // Verifica si se encontraron películas
-      if (data.Response === 'True') {
+        if (data.Response === 'True') {
         // Genera el HTML dinámicamente para mostrar la lista de películas
         const movieResultsContainer = document.getElementById('movieResults');
         movieResultsContainer.innerHTML = '';
 
         data.Search.forEach(movie => {
-          const movieCard = document.createElement('div');
-          movieCard.classList.add('movie-card');
-          movieCard.innerHTML = `
+            const movieCard = document.createElement('div');
+            movieCard.classList.add('movie-card');
+            movieCard.innerHTML = `
             <h2>${movie.Title}</h2>
             <p><strong>Año:</strong> ${movie.Year}</p>
             <p><strong>Tipo:</strong> ${movie.Type}</p>
             <img src="${movie.Poster}" alt="${movie.Title} Poster" class="img-fluid">
-          `;
-          movieResultsContainer.appendChild(movieCard);
+            `;
+            movieResultsContainer.appendChild(movieCard);
         });
-      } else {
+        } else {
         // Muestra un mensaje si no se encontraron películas
         alert('No se encontraron películas. Verifique el título.');
-      }
+        }
     } catch (error) {
       // Manejo de errores
-      console.error('Error al obtener datos de la API:', error);
-      alert('Ocurrió un error al obtener datos de la API.');
+        console.error('Error al obtener datos de la API:', error);
+        alert('Ocurrió un error al obtener datos de la API.');
     }
-  }
+    }
